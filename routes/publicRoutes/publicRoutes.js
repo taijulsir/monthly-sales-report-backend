f1c53efa4919bcc47ce5c0f06de173d6c8516b45
taijulsir <princetaijul123@gmail.com>
2025-07-30 01:17:03 +0600

    updated : routes

--- routes/publicRoutes/publicRoutes.js
+++ routes/publicRoutes/publicRoutes.js
@@ -2,14 +2,14 @@ import { Router } from 'express'
 import { getSaleAnalytics } from '#controllers/analytics/analytics.js'
 import { getMonthlyData } from '#controllers/reports/reports.js'
 import { getTopProductsByOrder } from '#controllers/topProducts/getProductsByOrder/getProductsByOrder.js'
-import { getTopProdcutsByShop } from '#controllers/topProducts/getProductsByShop/getProductsByShop.js'
+import { getTopProductsByShop } from '#controllers/topProducts/getProductsByShop/getProductsByShop.js'
 
 const publicRoutes = Router()
 
 publicRoutes.route('/reports/analytics/:startDate/:endDate').get(getMonthlyData)
 publicRoutes.route("/saleAnalytics/:year/:month").get(getSaleAnalytics)
 publicRoutes.route('/topProductsByOrder/:startDate/:endDate').get(getTopProductsByOrder)
-publicRoutes.route("/topProductsByShop/:year/:month").get(getTopProdcutsByShop)
+publicRoutes.route("/topProductsByShop/:year/:month").get(getTopProductsByShop)
 
 
 export default publicRoutes
