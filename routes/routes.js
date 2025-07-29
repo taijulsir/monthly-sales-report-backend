51407ec317fd248c73faf9717b850e3747b24c88
taijulsir <princetaijul123@gmail.com>
2025-07-30 02:30:07 +0600

    remove unused imports

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,6 @@
+import { insertData } from '#controllers/insertData/insertData.js'
 import { Router } from 'express'
 import publicRoutes from './publicRoutes/publicRoutes.js'
-import { insertData } from '#controllers/insertData/insertData.js'
-import { OrderItem } from '#models/orderItem/orderItemModel.js'
-import { Order } from '#models/order/orderModel.js'
-import { Product } from '#models/product/productModel.js'
 
 const routes = Router()
 
